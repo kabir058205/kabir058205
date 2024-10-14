2d3e57ebf990898c0fc611b089dc4d9ebd1cefdb
kabirsharanchandra <kabirmca@gmail.com>
2024-10-15 00:40:06 +0200

    arrange

--- 1 Day/1_variables.py
+++
@@ -1,7 +0,0 @@
-name = "kabir"
-age = 42
-price=45.34
-
-print ("My name is  : ", name)
-print ("My age is :", age)
-print ("My name and age is is  : ", name ," & " , age)
--- 1 Day/4_calculate.py
+++
@@ -1,9 +0,0 @@
-a = 5
-b = 7
-
-sum = a+b
-sub = b - a 
-print("Sum of a and b is " , sum) 
-print("Dif of a and b is " , sub) 
-
- 
