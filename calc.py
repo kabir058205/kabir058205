eeb748a91239024068e1d6faf301a2c6632946d5
kabirsharanchandra <kabirmca@gmail.com>
2026-01-07 18:28:17 +0100

    feature/pyspark

--- calc.py
+++
@@ -1,6 +0,0 @@
-First_input = float(input("Enter first number? "))
-Second_input = float(input("second Seco number? "))
-Sum = First_input + Second_input
-print( "Sum of both number is " + str(Sum))
-Sub = First_input - Second_input
-print( "Subtract of both number is " + str(Sub))
